ad08861ae39722ea4e09ab29a59431e87e770552
nikhildeora <fitnexy@gmail.com>
2023-05-11 14:11:28 +0530

    fetching of blogs

--- src/component/AllBlogs.jsx
+++ src/component/AllBlogs.jsx
@@ -50,7 +50,8 @@ const AllBlogs = () => {
         headers: {
           Authorization: `Bearer skv1jPkv4t00iimtrHsb2uUq9u5dr6S5KtNX0ecGzfywiaHKzoX4U9tkf5kFDTWQ0Sp0yxExjZKyr0HDwL6xS8pHrAl79Knt4ZIjQCQRJAfrBE6MsYiCnQCYzzgjuEj72MDFwXuuj3r05ZidafZpNb2aw1mSAUEDrIGgl25WkuZsb4i1HWkV`
         },
-      })
+      });
+      
       console.log(response);
     } catch (error) {
       console.log("err", error);
